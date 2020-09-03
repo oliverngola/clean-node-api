34af586190afa98e237f3b03e1c7a4a7f8e6a9d4
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-09-03 21:55:25 +0100

    move sut creation to a factory method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Should returns true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should returns false if validator returns true', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@gmail.com')
     expect(isEmailValid).toBe(false)
   })
