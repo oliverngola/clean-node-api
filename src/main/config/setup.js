6735a4b9accd203feea44d96bd73e998ba104628
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-09-26 21:27:12 +0100

    enable cors

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,9 @@
 module.exports = app => {
   app.disable('x-powered-by')
+  app.use((request, response, next) => {
+    response.set('access-origin-allow-control', '*')
+    response.set('access-origin-allow-methods', '*')
+    response.set('access-origin-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -10,4 +10,15 @@ describe('App test', () => {
     const response = await request(app).get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enabale CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      return res.json({})
+    })
+
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-origin-allow-control']).toBe('*')
+    expect(response.headers['access-origin-allow-methods']).toBe('*')
+    expect(response.headers['access-origin-allow-headers']).toBe('*')
+  })
 })
