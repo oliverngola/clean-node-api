3cedc83d9714c11d00fda5db8ca384f8bec3ba01
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-09-26 21:58:39 +0100

    move test-cors to its own file

---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('CORS Middleware', () => {
+  test('Should enabale CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      return res.json({})
+    })
+
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-origin-allow-control']).toBe('*')
+    expect(response.headers['access-origin-allow-methods']).toBe('*')
+    expect(response.headers['access-origin-allow-headers']).toBe('*')
+  })
+})
