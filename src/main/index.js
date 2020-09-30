87ad313a9bd9aa91fcb47f2239ce33f6931a2682
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2020-09-30 19:22:21 +0100

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,10 @@
-const app = require('./config/app')
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '.env') })
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('SERVER IS RUNNING'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(5858, () => console.log('SERVER IS RUNNING'))
+  }).catch(console.error)
